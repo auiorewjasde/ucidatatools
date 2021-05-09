5f652b8878d5f9b2b9ea06f83ca7585377ddaa89
a <a@a>
2021-05-09 22:05:31 +0900

    a

--- all.sh
+++ all.sh
@@ -5,3 +5,14 @@ python3 accel2svm.py _cutaruki_0_wo_title.csv 0 _data0.unsorted.txt _data0.sorte
 python3 accel2svm.py _cutaruki_1_wo_title.csv 1 _data1.unsorted.txt _data1.sorted.txt
 
 
+
+
+python3 accel2knn.py _cutaruki_0_wo_title.csv 0 _data0.knn.unsorted.txt _data0.knn.sorted.txt
+python3 accel2knn.py _cutaruki_1_wo_title.csv 1 _data1.knn.unsorted.txt _data1.knn.sorted.txt
+
+
+#cat _data0.knn.sorted.txt _data1.knn.sorted.txt > _dataX.knn.sorted.txt
+#sed 1d < _dataX.knn.sorted.txt > _dataX.knn.sorted.trai.txt
+#head -1 _dataX.knn.sorted.txt > _dataX.knn.sorted.test.txt
+#python3 k-nn.py 10 6144 _dataX.knn.sorted.trai.txt _dataX.knn.sorted.test.txt
+
---
+++ k-nn.py
@@ -0,0 +1,52 @@
+import sys
+
+
+k = int(sys.argv[1])
+vallen = int(sys.argv[2])
+
+trainingfile = sys.argv[3]
+testfile = sys.argv[4]
+
+def line2label_n_values(line):
+	#print(line,end="")
+	tokens = line.rstrip().split(",")
+
+	label = tokens[0]
+	vals = tokens[1:1+vallen]
+	#print([label,vals])
+	return [label,vals]
+
+def evalFn(vals0, l_n_vals1):
+	#print("l_n_vals1=", l_n_vals1)
+	vals1 = l_n_vals1[1]
+	#print(vals1)
+	dist = 0.0
+	for i in range(vallen):
+		dist += (float(vals0[i]) - float(vals1[i]))**2
+	return dist
+
+
+with open(trainingfile) as filtrain:
+	l_vals = []
+	lines = filtrain.readlines()
+	for line in lines:
+		l_n_v = line2label_n_values(line)
+		l_vals.append(l_n_v)
+
+with open(testfile) as filtest:
+	lines = filtest.readlines()
+	for line in lines:
+		label,values = line2label_n_values(line)
+
+		l_vals_s = sorted(l_vals, key=lambda x:evalFn(values,x))
+
+		#print(l_vals_s)
+		formax = min( len(l_vals_s), k)
+		
+		print("accu= ",label, " : ", end="")
+		for i in range(formax):
+			lv = l_vals[i]
+			#print(lv)
+			print(lv[0], " ", end="")
+		print()
+
