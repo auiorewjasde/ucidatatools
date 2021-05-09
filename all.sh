87e20e9e00185911744b34030c653d430154e9ab
a <a@a>
2021-05-09 19:59:41 +0900

    x

--- all.sh
+++ all.sh
@@ -1,6 +1,7 @@
 sed 1d < cutaruki_0.csv > _cutaruki_0_wo_title.csv
-sed 1d < cutaruki_1.csv > _cutaruki_1_wo_tilte.csv
+sed 1d < cutaruki_1.csv > _cutaruki_1_wo_title.csv
 
-python3 accel2svm.py _cutaruki_0_wo_title.csv _cutaruki_1_wo_tilte.csv _data.unsorted.txt
+python3 accel2svm.py _cutaruki_0_wo_title.csv 0 _data0.unsorted.txt _data0.sorted.txt
+python3 accel2svm.py _cutaruki_1_wo_title.csv 1 _data1.unsorted.txt _data1.sorted.txt
 
 
